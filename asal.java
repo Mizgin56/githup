2ca5be61d3238f84a56ac4f16a3be2d70e5393dd
Mizgin Elçiçek <101660818+Mizgin56@users.noreply.github.com>
2022-08-31 13:05:31 +0300

    Add files via upload

---
+++ asal.java
@@ -0,0 +1,23 @@
+package Kodluyoruz;
+
+public class asal {
+
+	public static void main(String[] args) {
+		// Asal sayý Bulma
+		
+		int sayac = 0;
+     
+		for (int i = 2; i <= 100; i++) {
+            for (int k = 2; k < i; k++) {
+                if (i % k == 0) {
+                    sayac++;
+                }
+            }
+            if (sayac == 0) {
+                System.out.println(i);
+            } 
+            sayac = 0;
+        }
+	}
+
+}
